93fd9610b9f7bb55194ae514c09409dbbcde25c6
onaxiz <onaxiz@tutanota.com>
2022-12-07 14:47:57 +0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Evgeniia Maksimova on 07.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
