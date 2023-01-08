7ffdd9661aaad2d2a46b14187f7978ecc9c2a1d0
onaxiz <onaxiz@tutanota.com>
2023-01-09 02:26:21 +0300

    Auto stash before revert of "Revert "Initial Commit""

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,19 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject var viewModel: EmojiMemoryGame
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            ScrollView {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 75))]) {
+                    ForEach(viewModel.cards) { card in
+                        CardView(card: card)
+                            .aspectRatio(2/3, contentMode: .fit)
+                            .onTapGesture {
+                                viewModel.choose(card)
+                            }
+                    }
+                }
+            }
+            .foregroundColor(/*@START_MENU_TOKEN@*/.red/*@END_MENU_TOKEN@*/)
         }
-        .padding()
+        .padding(.horizontal)
+    }
+    
+}
+
+struct CardView: View {
+    let card: MemoryGame<String>.Card
+    
+    var body: some View {
+        ZStack {
+            let shape = RoundedRectangle(cornerRadius: 20.0)
+            if card.isFaceUp {
+                shape.fill().foregroundColor(.white)
+                Text(card.content).font(.largeTitle)
+                shape.strokeBorder(lineWidth: 1)
+            } else if card.isMatched {
+                shape.opacity(0)
+            } else {
+                shape.fill()
+            }
+        }
     }
 }
 
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        let game = EmojiMemoryGame()
+        ContentView(viewModel: game)
+            .preferredColorScheme(.dark)
+        ContentView(viewModel: game)
+            .preferredColorScheme(.light)
     }
 }
