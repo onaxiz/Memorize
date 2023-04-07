29e8496d36cc088da8b999cf3caecabf719b9aa2
Evgeniya Maximova <onaxiz@tutanota.com>
2023-04-07 20:41:20 +0300

    add timer

---
+++ Memorize/Cardify.swift
@@ -0,0 +1,47 @@
+//
+//  Cardify.swift
+//  Memorize
+//
+//  Created by Evgeniia Maksimova on 03.03.2023.
+//
+
+import SwiftUI
+
+struct Cardify: AnimatableModifier {
+    
+    init(isFaceUp: Bool) {
+        rotation = isFaceUp ? 0 : 180
+    }
+    
+    var animatableData: Double {
+        get { rotation }
+        set { rotation = newValue }
+    }
+    var rotation: Double
+    
+    func body(content: Content) -> some View {
+        ZStack {
+            let shape = RoundedRectangle(cornerRadius: DrawingConstants.cornerRadius)
+            if rotation < 90 {
+                shape.fill().foregroundColor(.white)
+                shape.strokeBorder(lineWidth: DrawingConstants.lineWidth)
+            } else {
+                shape.fill()
+            }
+            content
+                .opacity(rotation < 90 ? 1 : 0)
+        }
+        .rotation3DEffect(Angle.degrees(rotation), axis: (0, 1, 0))
+    }
+    
+    private struct DrawingConstants {
+        static let cornerRadius: CGFloat = 10
+        static let lineWidth: CGFloat = 3
+    }
+}
+
+extension View {
+    func cardify(isFaceUp: Bool) -> some View {
+        return self.modifier(Cardify(isFaceUp: isFaceUp))
+    }
+}
---
+++ Memorize/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  Memorize
+//
+//  Created by Evgeniia Maksimova on 03.03.2023.
+//
+
+import Foundation
